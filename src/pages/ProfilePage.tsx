54a395b52208f1ec87238f83b1b7618288f63030
PEERLAPTOP\Tidha <tidhar.peer@gmail.com>
2026-05-29 22:58:20 +0300

    fix import

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
-import { ArrowLeft, Star, Flame, Copy, Check, Music2, Sun, Moon, CloudUpload, CheckCircle2 } from 'lucide-react'
+import { ArrowLeft, Star, Flame, Copy, Check, Music2, Sun, Moon, CloudUpload } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Separator } from '@/components/ui/separator'
